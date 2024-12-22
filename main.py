f6e83aec97f3a3346ee37babb1031f34a7ddfa1b
AllanDBD <123992798+AllanDBD@users.noreply.github.com>
2024-12-22 13:49:56 +0100

    main v2

--- main.py
+++ main.py
@@ -1,26 +1,9 @@
 import random
+import json
+from datetime import datetime
 
 class Graphe:
-    def __init__(self, n):
-        self.n = n
-        self.p = random.random()  # Probabilité aléatoire entre 0 et 1
-        self.graphe = self.generer_lists()
-        self.degre_maximum()
-
-    def generer_lists(self):
-        graphe = []
-        for i in range(self.n):
-            ligne = []
-            graphe.append(ligne)
-        for j in range(self.n):
-            for k in range(j+1,self.n):
-                if random.random() < self.p:
-                    graphe[j].append(k)
-                    graphe[k].append(j)
-        return graphe
-
     def afficher(self):
-        print(f"Probabilité p = {self.p:.2f}")
         for ligne in self.graphe:
             print(ligne)
 
@@ -55,10 +38,66 @@ class Graphe:
         
         print(compteur)
 
-n = int(input("Taille du graphe : "))
-graphe = Graphe(n)
+class Random_graphe(Graphe):
+    def __init__(self):
+        self.n = int(input("Taille du graphe : "))
+        self.p = random.random()  # Probabilité aléatoire entre 0 et 1
+        self.graphe = self.generer_lists()
+        self.degre_maximum()
+        self.save_graphe()
+
+    def generer_lists(self):
+        graphe = []
+        for i in range(self.n):
+            ligne = []
+            graphe.append(ligne)
+        for j in range(self.n):
+            for k in range(j+1,self.n):
+                if random.random() < self.p:
+                    graphe[j].append(k)
+                    graphe[k].append(j)
+        return graphe
+    
+    def save_graphe(self):
+        answer=input("Voulez-vous sauvegarder ce graphe aléatoire dans un fichier? (o/O si oui): ")
+        if(answer=="o" or answer=="O"):
+            answer=input("Souhaitez-vous choisir le nom du fichier où sauvegarder le graphe? (o/O si oui, n/N sinon): ")
+            if(answer=="o" or answer=="O"):
+                file_name=input("Entrez le nom du fichier: ")
+            elif(answer=="n" or answer=="N"):
+                current_time = datetime.now()
+                file_name = "random_graphe-"+current_time.strftime("%Y-%m-%d_%H-%M-%S") + ".json"
+            else:
+                return
+            graphe = {str(i): self.graphe[i] for i in range(self.n)}
+            data= {
+                "n": self.n,
+                "graphe": graphe
+            }
+            with open(file_name, "w") as file:
+                json.dump(data, file, indent=4)
+
+class Import_graphe(Graphe):
+    def __init__(self,file):
+        #self.file= str(input("Entrer le nom du fichier dont vous souhaitez importer le graphe: "))
+        # Lecture du fichier JSON
+        with open(file, "r") as file:
+            self.data = json.load(file)
+
+        self.n = self.data["n"]
+        self.graphe = self.import_listes()
+        self.degre_maximum()
+
+    def import_listes(self):    
+        lists = self.data["graphe"]
+        graphe=[]
+        for node in lists:
+            graphe.append(lists[node])
+        return graphe
+
+#graphe = Random_graphe()
+graphe = Import_graphe("test2.json")
 graphe.afficher()
-graphe.degre_maximum()
 graphe.afficher_degre_max()
 graphe.nb_sommets_par_degre()
 graphe.nb_chemins_induits_longueur_2()
